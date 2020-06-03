5179d57dcf9c1f7038e22db98a3377f878a93c85
HiralHirpara <HiralHirpara@users.noreply.github.com>
2020-06-03 12:11:20 -0700

    Initial check-in of module 1-FirstLab

---
+++ Exercise1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ------------ First Program ------------------------
+            String sum; String tryAgain; int sum2;
+            tryAgain = "y";
+            while (tryAgain == "y")
+            {
+                Random rnum = new Random();
+                var num1 = rnum.Next(1, 10);
+                var num2 = rnum.Next(1, 10);
+                Console.Write($"{num1} + {num2} = ");
+                sum = Console.ReadLine();
+                //Console.WriteLine($"{sum}");
+                sum2 = Convert.ToInt32(sum);
+                int total = num1 + num2;
+
+                if (total == sum2)
+                {
+                    Console.WriteLine("You are Correct");
+                }
+                else
+                {
+                    Console.WriteLine("You are not correct");
+                }
+                Console.Write($"Do you want to try again (yes or no)?  ");
+                tryAgain = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Exercise2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //------------- Second program --------------------------
+            Console.WriteLine("Enter list");
+            var list = Console.ReadLine().Split();
+            var accending = 1; var decending = 1;
+            for (int i = 0; i < list.Length - 1; i++)
+            {
+                if (int.Parse(list[i]) >= int.Parse(list[i + 1]))
+                {
+                    decending++;
+                }
+                else
+                {
+                    accending++;
+                }
+            }
+            if (accending == list.Length || decending == list.Length)
+            {
+                Console.WriteLine("Your list is sorted.");
+            }
+            else
+            {
+                Console.WriteLine("Your list is not sorted.");
+            }
+        }
+    }
+}
